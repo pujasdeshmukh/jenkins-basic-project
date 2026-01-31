caddc517e84f7f91730e9ac40a5f8eceadb079cc
Puja <newpuja00@gmail.com>
2026-02-01 01:17:50 +0530

    Poll SCM

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins on EC2!");
+        System.out.println("Hello triggered from Poll SCM!");
     }
 }
